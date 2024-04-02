37f3eeb014f5cdaef7ebd8f1292291067a9407be
jscheffner <jscheffner@users.noreply.github.com>
2024-04-02 20:23:10 +0200

    remove unused imports

--- src/components/CommandMenu.tsx
+++ src/components/CommandMenu.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { DeclareExchangeDialog } from './DeclareExchangeDialog'
 import {
   CommandDialog,
